43776881d116bd2318fdba067d11d640d2a728d6
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2025-02-19 11:30:41 +0100

    MemoryBus med 65536 bytes

--- src/memory_bus.rs
+++ src/memory_bus.rs
@@ -1,12 +1,15 @@
 pub struct MemoryBus {
-    memory: [u8; 0xFFFF]
+    memory: [u8; 65536] // fra 0x0000 til 0xFFFF
 }
 
 impl MemoryBus {
+    pub fn new() -> Self {
+        Self { memory: [0; 65536] }
+    }
     pub fn read_byte(&self, address: u16) -> u8 {
         self.memory[address as usize]
     }
     pub fn write_byte(&mut self, address: u16, byte: u8) {
-
+        self.memory[address as usize] = byte;
     }
 }
