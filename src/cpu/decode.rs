166b90cd34ab43149041555818055efea3eff130
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2025-03-11 10:09:42 +0100

    Samler 16-bitsregister i read_16 og write_16

--- src/cpu/decode.rs
+++ src/cpu/decode.rs
@@ -1,12 +1,14 @@
 use crate::cpu::CPU;
+use crate::registers::Reg16::BC;
 
 impl CPU {
     pub fn call(&mut self) -> u32 {
         let opcode = self.fetch_byte();
         match opcode {
             0x00 => { 1 },
-            0x01 => { let word = self.fetch_word(); self.registers.set_bc(word); 3 }
-            0x02 => { self.bus.write_byte(self.registers.get_bc(), self.registers.a); 2 }
+            0x01 => { let word = self.fetch_word(); self.registers.write_16(BC, word); 3 }
+            0x02 => { self.bus.write_byte(self.registers.read_16(BC), self.registers.a); 2 }
+            0x03 => { self.registers.write_16(BC, self.registers.read_16(BC).wrapping_add(1)); 2 }
             _ => todo!("Instruksjonen er ikke støttet!")
         }
     }
--- src/registers.rs
+++ src/registers.rs
@@ -11,37 +11,40 @@ pub struct Registers {
     pub l: u8,
 }
 
+pub enum Reg16 {
+    AF,
+    BC,
+    DE,
+    HL,
+}
+
 impl Registers {
-    pub fn get_af(&self) -> u16 {
-        (self.a as u16) << 8
-            | (u8::from(self.f) as u16)
+    pub fn read_16(&self, reg: Reg16) -> u16 {
+        match reg {
+            Reg16::AF => ((self.a as u16) << 8) | (u8::from(self.f) as u16),
+            Reg16::BC => ((self.b as u16) << 8) | (self.c as u16),
+            Reg16::DE => ((self.d as u16) << 8) | (self.e as u16),
+            Reg16::HL => ((self.h as u16) << 8) | (self.l as u16),
+        }
     }
-    pub fn set_af(&mut self, value: u16) {
-        self.a = ((value & 0xFF00) >> 8) as u8;
-        self.f = FlagsRegister::from((value & 0xF0) as u8);
-    }
-    pub fn get_bc(&self) -> u16 {
-        (self.b as u16) << 8
-            | (self.c as u16)
-    }
-    pub fn set_bc(&mut self, value: u16) {
-        self.b = ((value & 0xFF00) >> 8) as u8;
-        self.c = (value & 0xFF ) as u8;
-    }
-    pub fn get_de(&self) -> u16 {
-        (self.d as u16) << 8
-            | (self.e as u16)
-    }
-    pub fn set_de(&mut self, value: u16) {
-        self.d = ((value & 0xFF00) >> 8) as u8;
-        self.e = (value & 0xFF ) as u8;
-    }
-    pub fn get_hl(&self) -> u16 {
-        (self.h as u16) << 8
-            | (self.l as u16)
-    }
-    pub fn set_hl(&mut self, value: u16) {
-        self.h = ((value & 0xFF00) >> 8) as u8;
-        self.l = (value & 0xFF ) as u8;
+    pub fn write_16(&mut self, reg: Reg16, value: u16) {
+        match reg {
+            Reg16::AF => {
+                self.a = ((value & 0xFF00) >> 8) as u8;
+                self.f = FlagsRegister::from((value & 0xF0) as u8);
+            }
+            Reg16::BC => {
+                self.b = ((value & 0xFF00) >> 8) as u8;
+                self.c = (value & 0xFF ) as u8;
+            }
+            Reg16::DE => {
+                self.d = ((value & 0xFF00) >> 8) as u8;
+                self.e = (value & 0xFF ) as u8;
+            }
+            Reg16::HL => {
+                self.h = ((value & 0xFF00) >> 8) as u8;
+                self.l = (value & 0xFF ) as u8;
+            }
+        }
     }
 }
