27067f6774645822691caf9f3eaa59a0153f27fa
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2025-03-12 15:00:11 +0100

    Fjerner forsøk på tidl. implementasjon, drar inn Pixels og spinner opp vindu

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,58 @@
+use pixels::{Error, Pixels, SurfaceTexture};
+use winit::dpi::LogicalSize;
+use winit::event::{Event, WindowEvent};
+use winit::event_loop::EventLoop;
+use winit::keyboard::KeyCode;
+use winit::window::WindowBuilder;
+use winit_input_helper::WinitInputHelper;
+
 mod cpu;
 mod flags_register;
-mod instruction;
 mod memory_bus;
 mod registers;
 
-fn main() {
-    println!("Hello, world!");
-    println!("true as u8: {}", true as u8);
-    println!("false as u8: {}", false as u8);
+const SCREEN_WIDTH: u32 = 160;
+const SCREEN_HEIGHT: u32 = 144;
+
+fn main() -> Result<(), Error> {
+    let event_loop = EventLoop::new().unwrap();
+    let mut input = WinitInputHelper::new();
+    let window = {
+        let size = LogicalSize::new(SCREEN_WIDTH as f64, SCREEN_HEIGHT as f64);
+        WindowBuilder::new()
+            .with_title("GameBoy")
+            .with_inner_size(size)
+            .with_min_inner_size(size)
+            .build(&event_loop)
+            .unwrap()
+    };
+    
+    let mut pixels = {
+        let window_size = window.inner_size();
+        let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
+        Pixels::new(SCREEN_WIDTH, SCREEN_HEIGHT, surface_texture)?
+    };
+    
+    let res = event_loop.run(|event, elwt| {
+        if let Event::WindowEvent { 
+            event: WindowEvent::RedrawRequested,
+            ..
+        } = event
+        {
+            if let Err(err) = pixels.render() {
+                elwt.exit();
+                return;
+            }
+        }
+        
+        if input.update(&event) {
+            if input.key_pressed(KeyCode::Escape) || input.close_requested() {
+                elwt.exit();
+                return;
+            }
+            
+            window.request_redraw();
+        }
+    });
+    res.map_err(|e| Error::UserDefined(Box::new(e)))
 }
