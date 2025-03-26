8b4eefd56f8ec5f9fef68164b1465454b8d49667
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2025-03-26 22:03:33 +0100

    AddressBus: Tar i bruk MBC-operasjoner, bruker debug, setter gyldig verdi etter bootrom

--- src/address_bus.rs
+++ src/address_bus.rs
@@ -1,4 +1,4 @@
-use log::info;
+use log::debug;
 use crate::bootrom::Bootrom;
 use crate::cartridge::Cartridge;
 use crate::joypad::Joypad;
@@ -22,7 +22,7 @@ pub struct AddressBus {
 
 impl AddressBus {
     pub fn new(cart: Cartridge) -> Self {
-        Self {
+        let mut address_bus = Self {
             cartridge: cart,
             ppu: PPU::new(),
             work_ram: [0; WORK_RAM_SIZE],
@@ -32,7 +32,11 @@ impl AddressBus {
             joypad: Joypad::new(),
             bootrom: Bootrom::new(),
             timer: Timer::new(),
+        };
+        if cfg!(feature = "test") {
+            address_bus.set_initial();
         }
+        address_bus
     }
     pub fn cycle(&mut self, t_cycles: u32) {
         self.timer.cycle(t_cycles);
@@ -52,7 +56,7 @@ impl AddressBus {
             0x0000 ..= 0x00ff if self.bootrom.is_active() => self.bootrom[address],
             0x0000 ..= 0x7fff => self.cartridge.mbc.read_rom(address),
             0x8000 ..= 0x9fff => self.ppu.video_ram[address as usize & 0x1FFF],
-            0xa000 ..= 0xbfff => todo!("Have not implemented extra RAM at 0xA000 to 0xBFFF"),
+            0xa000 ..= 0xbfff => self.cartridge.mbc.read_ram(address),
             0xc000 ..= 0xcfff | 0xe000 ..= 0xefff => self.work_ram[address as usize & 0x1fff],
             0xd000 ..= 0xdfff | 0xf000 ..= 0xfdff => self.work_ram[address as usize & 0x1fff],
             0xfe00 ..= 0xfe9f => self.ppu.read_oam(address),
@@ -60,7 +64,8 @@ impl AddressBus {
             0xff00 ..= 0xff7f => self.io_read_byte((address & 0x00ff) as u8),
             0xff80 ..= 0xfffe => self.high_ram[address as usize & 0x007F],
             0xffff => self.interrupt_enable_register,
-            _ => unreachable!()
+
+            _ => { debug!("Minneadresse {:#04x} kan ikke leses fra. Returnerer 0xff.", address); 0xff }
         }
     }
     pub fn io_read_byte(&self, address: u8) -> u8 {
@@ -75,23 +80,23 @@ impl AddressBus {
             0x4f => panic!("VRAM Bank Select is CGB feature"),
             0x50 => panic!("write-only"),
             0x51 ..= 0x70 => panic!("Game Boy Color feature"),
-            _ => unreachable!()
+            _ => { debug!("IO-minneadresse {:#04x} kan ikke leses fra. Returnerer 0xff.", address); 0xff }
         }
     }
     pub fn write_byte(&mut self, address: u16, byte: u8) {
         match address {
             0x0000 ..= 0x00ff if self.bootrom.is_active() => (),
-            0x0000 ..= 0x7fff => { info!("MBC0 is read-only. Tried writing at {:#06x}", address); },
+            0x0000 ..= 0x7fff => self.cartridge.mbc.write_rom(address, byte),
             0x8000 ..= 0x9fff => self.ppu.video_ram[address as usize  & 0x1FFF] = byte,
-            0xa000 ..= 0xbfff => panic!("MBC0 is read-only"),
+            0xa000 ..= 0xbfff => self.cartridge.mbc.write_ram(address, byte),
             0xc000 ..= 0xcfff | 0xe000 ..= 0xefff => self.work_ram[address as usize & 0x1fff] = byte,
             0xd000 ..= 0xdfff | 0xf000 ..= 0xfdff => self.work_ram[address as usize & 0x1fff] = byte,
             0xfe00 ..= 0xfe9f => self.ppu.write_oam(address, byte),
-            0xfea0 ..= 0xfeff => panic!("Not usable!"),
+            0xfea0 ..= 0xfeff => debug!("Not usable!"),
             0xff00 ..= 0xff7f => self.io_write_byte((address & 0x00ff) as u8, byte),
             0xff80 ..= 0xfffe => self.high_ram[address as usize & 0x007F] = byte,
             0xffff => self.interrupt_enable_register = byte,
-            _ => unreachable!()
+            _ => debug!("Minneadresse {:#04x} kan ikke skrives til.", address)
         }
     }
     pub fn io_write_byte(&mut self, address: u8, byte: u8) {
@@ -101,13 +106,13 @@ impl AddressBus {
             0x04 ..= 0x07 => self.timer.write_byte(address, byte),
             0x0f => self.interrupt_flag = byte,
             0x10 ..= 0x26 => (), // Audio not implemented
-            0x30 ..= 0x3f => panic!("Wave pattern not implemented"),
+            0x30 ..= 0x3f => debug!("Wave pattern not implemented"),
             0x40 ..= 0x45 | 0x47 ..= 0x4b => self.ppu.write_byte(address, byte),
             0x46 => self.oam_dma(byte),
-            0x4f => panic!("VRAM Bank Select is CGB feature"),
+            0x4f => debug!("VRAM Bank Select is CGB feature"),
             0x50 => self.bootrom.deactivate(),
-            0x51 ..= 0x70 => panic!("Game Boy Color feature"),
-            _ => unreachable!()
+            0x51 ..= 0x70 => debug!("Game Boy Color feature ved IO-minneadresse 0xff{:02x}", address),
+            _ => debug!("IO-minneadresse 0xff{:02x} kan ikke skrives til.", address)
         }
     }
     pub fn read_word(&self, address: u16) -> u16 {
@@ -130,5 +135,39 @@ impl AddressBus {
             let sprite = self.read_byte(base + i);
             self.ppu.dma_write_oam(i, sprite);
         }
+    }
+    #[cfg(feature = "test")]
+    fn set_initial(&mut self) {
+        self.write_byte(0xFF05, 0);
+        self.write_byte(0xFF06, 0);
+        self.write_byte(0xFF07, 0);
+        self.write_byte(0xFF10, 0x80);
+        self.write_byte(0xFF11, 0xBF);
+        self.write_byte(0xFF12, 0xF3);
+        self.write_byte(0xFF14, 0xBF);
+        self.write_byte(0xFF16, 0x3F);
+        self.write_byte(0xFF16, 0x3F);
+        self.write_byte(0xFF17, 0);
+        self.write_byte(0xFF19, 0xBF);
+        self.write_byte(0xFF1A, 0x7F);
+        self.write_byte(0xFF1B, 0xFF);
+        self.write_byte(0xFF1C, 0x9F);
+        self.write_byte(0xFF1E, 0xFF);
+        self.write_byte(0xFF20, 0xFF);
+        self.write_byte(0xFF21, 0);
+        self.write_byte(0xFF22, 0);
+        self.write_byte(0xFF23, 0xBF);
+        self.write_byte(0xFF24, 0x77);
+        self.write_byte(0xFF25, 0xF3);
+        self.write_byte(0xFF26, 0xF1);
+        self.write_byte(0xFF40, 0x91);
+        self.write_byte(0xFF42, 0);
+        self.write_byte(0xFF43, 0);
+        self.write_byte(0xFF45, 0);
+        self.write_byte(0xFF47, 0xFC);
+        self.write_byte(0xFF48, 0xFF);
+        self.write_byte(0xFF49, 0xFF);
+        self.write_byte(0xFF4A, 0);
+        self.write_byte(0xFF4B, 0);
     }
 }
