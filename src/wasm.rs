7f9309216f6af6666e3462d17175cbdce38a4101
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2026-04-09 12:38:39 +0200

    Forenkler WASM-oppsett, inspirert av pixels/examples/minimal-web

--- src/wasm.rs
+++ src/wasm.rs
@@ -1,11 +1,13 @@
 use log::info;
 use pixels::{PixelsBuilder, SurfaceTexture};
+use std::rc::Rc;
 use wasm_bindgen::prelude::*;
-use winit::dpi::PhysicalSize;
+use wasm_bindgen::JsCast;
+use winit::dpi::LogicalSize;
 use winit::event::{ElementState, Event, WindowEvent};
-use winit::event_loop::{ControlFlow, EventLoop};
+use winit::event_loop::EventLoop;
 use winit::keyboard::{Key, NamedKey};
-use winit::platform::web::{EventLoopExtWebSys, WindowBuilderExtWebSys};
+use winit::platform::web::WindowExtWebSys;
 use winit::window::WindowBuilder;
 
 use crate::frame_buffer::FrameBuffer;
@@ -14,6 +16,14 @@ use crate::joypad::JoypadKey;
 
 const SCREEN_WIDTH: u32 = 160;
 const SCREEN_HEIGHT: u32 = 144;
+
+fn get_window_size() -> LogicalSize<f64> {
+    let client_window = web_sys::window().unwrap();
+    LogicalSize::new(
+        client_window.inner_width().unwrap().as_f64().unwrap(),
+        client_window.inner_height().unwrap().as_f64().unwrap(),
+    )
+}
 
 fn js_key_to_joypad(key: Key<&str>) -> Option<JoypadKey> {
     match key {
@@ -41,59 +51,70 @@ pub fn start_emulator(rom_data: &[u8]) {
             .expect("Kunne ikke laste ROM");
 
         let event_loop = EventLoop::new().unwrap();
-        event_loop.set_control_flow(ControlFlow::Poll);
 
-        let canvas = web_sys::window()
-            .and_then(|w| w.document())
-            .and_then(|d| d.get_element_by_id("canvas"))
-            .and_then(|e| e.dyn_into::<web_sys::HtmlCanvasElement>().ok())
-            .expect("Fant ikke canvas-element med id 'canvas'");
-
-        let canvas_width = canvas.width();
-        let canvas_height = canvas.height();
-        info!("Canvas-størrelse: {}x{}", canvas_width, canvas_height);
-
-        let window: &'static winit::window::Window = Box::leak(Box::new(
+        let window = Rc::new(
             WindowBuilder::new()
-                .with_canvas(Some(canvas))
+                .with_title("Game Boy Emulator")
+                .with_inner_size(LogicalSize::new(SCREEN_WIDTH as f64, SCREEN_HEIGHT as f64))
+                .with_min_inner_size(LogicalSize::new(SCREEN_WIDTH as f64, SCREEN_HEIGHT as f64))
                 .build(&event_loop)
-                .unwrap()
-        ));
+                .unwrap(),
+        );
 
-        // Sett vindusstørrelse eksplisitt etter bygging
-        let _ = window.request_inner_size(PhysicalSize::new(SCREEN_WIDTH, SCREEN_HEIGHT));
+        // Legg winit-canvas til DOM
+        web_sys::window()
+            .and_then(|win| win.document())
+            .and_then(|doc| doc.get_element_by_id("screen"))
+            .and_then(|container| {
+                container
+                    .append_child(&web_sys::Element::from(window.canvas().unwrap()))
+                    .ok()
+            })
+            .expect("Kunne ikke legge canvas til DOM");
 
-        let window_size = window.inner_size();
-        info!("Vindusstørrelse: {}x{}", window_size.width, window_size.height);
+        // Lytt på resize-events fra nettleseren
+        let resize_closure = wasm_bindgen::closure::Closure::wrap(Box::new({
+            let window = Rc::clone(&window);
+            move |_e: web_sys::Event| {
+                let _ = window.request_inner_size(get_window_size());
+            }
+        }) as Box<dyn FnMut(_)>);
+        web_sys::window()
+            .unwrap()
+            .add_event_listener_with_callback("resize", resize_closure.as_ref().unchecked_ref())
+            .unwrap();
+        resize_closure.forget();
 
-        // Bruk kjente dimensjoner for surface, ikke window.inner_size() som kan være 0
-        let surface_width = if window_size.width > 0 { window_size.width } else { SCREEN_WIDTH };
-        let surface_height = if window_size.height > 0 { window_size.height } else { SCREEN_HEIGHT };
-        info!("Surface-størrelse: {}x{}", surface_width, surface_height);
+        // Sett initial størrelse til nettleservinduet
+        let _ = window.request_inner_size(get_window_size());
 
-        let surface_texture = SurfaceTexture::new(surface_width, surface_height, window);
+        let window_size = get_window_size().to_physical::<u32>(window.scale_factor());
+        let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &*window);
+        let texture_format = pixels::wgpu::TextureFormat::Rgba8Unorm;
         let mut pixels = PixelsBuilder::new(SCREEN_WIDTH, SCREEN_HEIGHT, surface_texture)
-            .wgpu_backend(pixels::wgpu::Backends::GL)
+            .texture_format(texture_format)
+            .surface_texture_format(texture_format)
             .build_async()
             .await
             .expect("Kunne ikke opprette Pixels");
 
         info!("Pixels opprettet, starter emulering");
 
-        // Game Boy CPU: 4194304 Hz -> 4194.304 t-sykluser per millisekund
         const CYCLES_PER_MS: f64 = 4194304.0 / 1000.0;
         let performance = web_sys::window().unwrap().performance().unwrap();
         let mut last_timestamp = performance.now();
         let mut cycle_debt: f64 = 0.0;
 
-        event_loop.spawn(move |event, _elwt| {
-            match &event {
+        let window = Rc::clone(&window);
+        #[allow(deprecated)]
+        event_loop.run(move |event, elwt| {
+            match event {
                 Event::AboutToWait => {
                     let now = performance.now();
                     let elapsed_ms = now - last_timestamp;
                     last_timestamp = now;
 
-                    // Begrens til maks 33ms (30 FPS) for å unngå spiral ved fryser/tab-bytte
+                    // Begrens til maks 33ms for å unngå spiral ved fryser/tab-bytte
                     let capped_ms = elapsed_ms.min(33.0);
                     cycle_debt += capped_ms * CYCLES_PER_MS;
 
@@ -112,6 +133,14 @@ pub fn start_emulator(rom_data: &[u8]) {
 
                     window.request_redraw();
                 }
+                Event::WindowEvent { event: WindowEvent::Resized(size), .. } => {
+                    if size.width > 0 && size.height > 0 {
+                        if let Err(err) = pixels.resize_surface(size.width, size.height) {
+                            log::error!("pixels.resize_surface() feilet: {err}");
+                            elwt.exit();
+                        }
+                    }
+                }
                 Event::WindowEvent { event: WindowEvent::KeyboardInput { event: key_event, .. }, .. } => {
                     match (key_event.state, key_event.logical_key.as_ref()) {
                         (ElementState::Pressed, key) => {
@@ -128,6 +157,6 @@ pub fn start_emulator(rom_data: &[u8]) {
                 }
                 _ => {}
             }
-        });
+        }).unwrap();
     });
 }
