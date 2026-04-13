46e8e3cc63ba266bf9d4ff33e605b9fc5d206b14
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2026-04-13 07:57:00 +0200

    Fjerner info-logger

--- web/src/local_storage_battery_save.rs
+++ web/src/local_storage_battery_save.rs
@@ -1,5 +1,5 @@
 use base64::prelude::*;
-use log::{error, info};
+use log::error;
 use gameboy_core::battery_save::BatterySave;
 
 pub struct LocalStorageBatterySave {
@@ -14,29 +14,25 @@ impl LocalStorageBatterySave {
 
 impl BatterySave for LocalStorageBatterySave {
     fn load(&self, ram: &mut [u8]) {
-        info!("Laster lagret spill fra LocalStorage (nøkkel: {})", self.local_storage_key);
         let local_storage = web_sys::window().unwrap().local_storage().unwrap().unwrap();
 
         match local_storage.get_item(&self.local_storage_key) {
             Ok(Some(value)) => {
                 let decoded = BASE64_STANDARD.decode(value).unwrap();
-                info!("Fant lagret data ({} bytes)", decoded.len());
-
                 ram.copy_from_slice(&decoded);
             }
-            Ok(None) => {
-                info!("Ingen lagret data funnet for nøkkel: {}", self.local_storage_key);
-            }
-            Err(err) => {
-                error!("Kunne ikke lese fra LocalStorage: {:?}", err);
+            Ok(None) => {}
+            Err(error) => {
+                error!("Klarte ikke å lese lagret spill fra LocalStorage: {:?}", error);
             }
         }
     }
 
     fn save(&self, data: &[u8]) {
-        info!("Lagrer spill til LocalStorage (nøkkel: {})", self.local_storage_key);
         let local_storage = web_sys::window().unwrap().local_storage().unwrap().unwrap();
         let encoded = BASE64_STANDARD.encode(data);
-        local_storage.set_item(&self.local_storage_key, &encoded).expect("Klarte ikke å lagre spill til LocalStorage");
+        if let Err(error) = local_storage.set_item(&self.local_storage_key, &encoded) {
+            error!("Klarte ikke å lagre spill til LocalStorage: {:?}", error)
+        };
     }
 }
