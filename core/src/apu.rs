ee8bf2f0eb0e301e9117279ba1b229ade528ea74
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2026-04-30 13:30:32 +0200

    Audio master control

--- core/src/apu.rs
+++ core/src/apu.rs
@@ -1,21 +1,28 @@
 
 pub struct APU {
-    
+    audio_master_control: u8,
 }
 
 impl APU {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            audio_master_control: 0,
+        }
     }
     pub fn cycle(&mut self, _t_cycles: u32) {
 
     }
-    pub fn read_byte(&self, _address: u8) -> u8 {
-        // Audio not implemented
-        0x00
+    pub fn read_byte(&self, address: u8) -> u8 {
+        match address {
+            0x26 => self.audio_master_control,
+            _ => 0x00 // Other audio channels not implemented
+        }
     }
-    pub fn write_byte(&self, _address: u8, _value: u8) {
-        // Audio not implemented
+    pub fn write_byte(&mut self, address: u8, value: u8) {
+        match address {
+            0x26 => self.audio_master_control &= (value | 0b1000000),
+            _ => {} // Other audio channels not implemented
+        }
     }
     pub fn read_wave_byte(&self, _address: u8) -> u8 {
         // Wave pattern not implemented
