211fc095754e66402271fb414539e4d0e63a7502
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2026-05-07 13:27:03 +0200

    struct PulseChannel

--- core/src/apu.rs
+++ core/src/apu.rs
@@ -1,8 +1,10 @@
+use log::info;
 
 pub struct APU {
     master_volume: u8,
     sound_panning: u8,
     audio_master_control: u8,
+    channel_2: PulseChannel,
 }
 
 impl APU {
@@ -11,13 +13,16 @@ impl APU {
             master_volume: 0,
             sound_panning: 0,
             audio_master_control: 0,
+            channel_2: PulseChannel {}
         }
     }
     pub fn cycle(&mut self, _t_cycles: u32) {
 
     }
     pub fn read_byte(&self, address: u8) -> u8 {
+        info!("Leser lyd-byte fra {:02x}", address);
         match address {
+            0x16..=0x19 => self.channel_2.read_byte(address),
             0x24 => self.master_volume,
             0x25 => self.sound_panning,
             0x26 => self.audio_master_control,
@@ -25,7 +30,9 @@ impl APU {
         }
     }
     pub fn write_byte(&mut self, address: u8, value: u8) {
+        info!("Skriver lyd-byte til {:02x}", address);
         match address {
+            0x16..=0x19 => self.channel_2.write_byte(address, value),
             0x24 => self.master_volume = value & 0b01110111,
             0x25 => self.audio_master_control = value,
             0x26 => self.audio_master_control = value & 0b1000000,
@@ -38,5 +45,16 @@ impl APU {
     }
     pub fn write_wave_byte(&self, _address: u8, _value: u8) {
         // Wave pattern not implemented
+    }
+}
+
+struct PulseChannel;
+
+impl PulseChannel {
+    fn read_byte(&self, address: u8) -> u8 {
+        0
+    }
+    fn write_byte(&mut self, address: u8, value: u8) {
+
     }
 }
