855dea18f6dbeb4f19063302b500ddbc65c86548
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2026-05-10 21:48:54 +0200

    Lyd-registre er read-only hvis APU er deaktivert

--- core/src/apu.rs
+++ core/src/apu.rs
@@ -26,12 +26,20 @@ impl APU {
         }
     }
     pub fn write_byte(&mut self, address: u8, value: u8) {
+        if !self.enabled {
+            if address == 0x26 && value & 0b1000_0000 != 0 {
+                self.enabled = true;
+                return;
+            } else {
+                return;
+            }
+        }
         info!("Skriver lyd-byte til {:02x}", address);
         match address {
             0x16..=0x19 => self.channel_2.write_byte(address, value),
             0x24 => self.master_volume = value & 0b0111_0111,
             0x25 => self.sound_panning = value,
-            0x26 => self.enabled = value & 0b100_0000 != 0,
+            0x26 => self.enabled = value & 0b1000_0000 != 0,
             _ => {} // Other audio channels not implemented
         }
     }
