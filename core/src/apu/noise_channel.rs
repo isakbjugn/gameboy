23d53137c25521fe1e809df79d78315c69799ada
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2026-05-28 13:49:50 +0200

    Lese-/skrivemetoder for 0x20 (length timer)

--- core/src/apu/noise_channel.rs
+++ core/src/apu/noise_channel.rs
@@ -1,12 +1,21 @@
+use crate::apu::length_timer::LengthTimer;
 
 #[derive(Default)]
-pub struct NoiseChannel;
+pub struct NoiseChannel {
+    length_timer: LengthTimer,
+}
 
 impl NoiseChannel {
     pub fn read_byte(&self, address: u8) -> u8 {
-        0
+        match address {
+            0x20 => panic!("FF20 er write-only"),
+            _ => 0x00
+        }
     }
-    pub fn write_byte(&self, address: u8, value: u8) {
-
+    pub fn write_byte(&mut self, address: u8, value: u8) {
+        match address {
+            0x20 => self.length_timer.load(value & 0b0011_1111),
+            _ => {}
+        }
     }
 }
