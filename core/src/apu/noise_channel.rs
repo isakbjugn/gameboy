15b18838614f4608477a694204c96c838d4da4dc
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2026-05-28 13:47:23 +0200

    Tomme lese- og skrive-metoder

--- core/src/apu/noise_channel.rs
+++ core/src/apu/noise_channel.rs
@@ -1,3 +1,12 @@
 
 #[derive(Default)]
 pub struct NoiseChannel;
+
+impl NoiseChannel {
+    pub fn read_byte(&self, address: u8) -> u8 {
+        0
+    }
+    pub fn write_byte(&self, address: u8, value: u8) {
+
+    }
+}
